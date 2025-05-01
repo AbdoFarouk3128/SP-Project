59cc951d1a82adf57d90cb29185c11ec14b17ce3
rahma <20241700303@cis.asu.edu.eg>
2025-05-01 03:11:34 +0300

    111

---
+++ SP Project/traiermenu.cpp
@@ -0,0 +1,10 @@
+#include "traiermenu.h"
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew SPProject::traiermenu);
+	return 0;
+}
